6179051512cac7f446a7a721187e74673e0f3e1b
graememccall <graememccall@hotmail.com>
2023-02-21 21:04:43 +0000

    More updates

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Article } from './article';
@@ -8,15 +9,19 @@ import { ARTICLES } from './mock-articles';
 })
 export class ArticleService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getArticles(): Observable<Article[]> {
-    const articles: Article[] = ARTICLES;
-    return of(articles);
+    //const articles: Article[] = ARTICLES;
+    //return of(articles);
+
+    return this.http.get<Article[]>("http://localhost:8000/articles");
   }
 
   getArticle(key: string): Observable<Article>{
-    const articles: Article[] = ARTICLES.filter(a => a.key === key);
-    return of(articles[0]);
+    //const articles: Article[] = ARTICLES.filter(a => a.key === key);
+    //return of(articles[0]);
+
+    return this.http.get<Article>("http://localhost:8000/article/" + key);
   }
 }
